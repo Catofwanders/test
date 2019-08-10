c2be50a18b3806446d449bec27177e881daa403c
DevPHP <baturataras@gmail.com>
2019-08-10 13:15:37 +0300

    init

---
+++ wp-content/plugins/weatherapp/admin/partials/weatherapp-admin-display.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       weatherapp
+ * @since      1.0.0
+ *
+ * @package    Weatherapp
+ * @subpackage Weatherapp/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
+<div class="wrap">
+    <input type="text" name="weather_city" placeholder="Enter city">
+</div>
---
+++ wp-content/plugins/weatherapp/includes/class-weatherapp-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       weatherapp
+ * @since      1.0.0
+ *
+ * @package    Weatherapp
+ * @subpackage Weatherapp/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Weatherapp
+ * @subpackage Weatherapp/includes
+ * @author     weatherapp <weatherapp>
+ */
+class Weatherapp_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
